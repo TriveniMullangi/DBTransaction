6b48905639a945d58020e72e6b9c11cd9d394a5e
Triveni <mullangitriveni4@gmail.com>
2019-01-23 17:08:50 +0530

    afterCommit function used

--- services/transaction.service.js
+++ services/transaction.service.js
@@ -19,25 +19,44 @@ var addUser = async (req, res, next) => {
     //let transaction;
     try {
       return  db.transaction((t)=>{
+          t.afterCommit(()=>{
+            return user1Model.User1.findAll()
+            .then(result=>{
+                console.log(JSON.stringify(result))
+            })  
+          })
             return user1Model.User1.create(req.body,{ transaction:t})
             .then(function (user) {
             //    // console.log(user.id)
             //     console.log("inserted in 1st table")
             //     console.log(JSON.stringify(user))
-            throw new Err("hi")
-            // return  user2Model.User2.create({
-            //     id:user.id,
-            //     firstName:user.firstName,
-            //     lastName:user.lastName,
-            //     city: user.city,
-            //     state:user.state,
-            //     country:user.country    
-            //   },{ transaction:t})
+            //throw new Err("hi")
+            console.log("inseryed in 1st table")
+            return  user2Model.User2.create({
+                id:user.id,
+                firstName:user.firstName,
+                lastName:user.lastName,
+                city: user.city,
+                state:user.state,
+                country:user.country    
+              },{ transaction:t})
+              
+            }).then((user1)=>{
+                console.log("inseryed in 2nd table")
+               // throw new Err("hi")
+                return  user3Model.User3.create({
+                    id:user1.id,
+                    firstName:user1.firstName,
+                    lastName:user1.lastName,
+                    city: user1.city,
+                    state:user1.state,
+                    country:user1.country    
+                  },{ transaction:t})
             });
             
         }) .then(function (result) {
             //transaction.commit();
-            res.send("successfully inserted in 2 tables")
+            res.send("successfully inserted in 3 tables")
           }).catch(()=>{
            // t.rollback()
             res.send("rolled back")
